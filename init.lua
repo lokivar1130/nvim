76d8b06d57c41408fb5a5360b69d635a323bb937
alexandrou-rousou <alexandrou.rousou@pairpoint.io>
2025-05-27 13:13:38 +0100

    disabled arrows and backspace

--- init.lua
+++ init.lua
@@ -1,23 +1,44 @@
 require("config.lazy")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 vim.api.nvim_create_autocmd("FileType", {
-	callback = function(args)
-		local ft = args.match
-		local ftplugin = string.format(vim.fn.stdpath("config") .. "/after/ftplugin/%s.lua", ft)
+  callback = function(args)
+    local ft = args.match
+    local ftplugin = string.format(vim.fn.stdpath("config") .. "/after/ftplugin/%s.lua", ft)
 
-		-- If there's no filetype-specific plugin, source default
-		if vim.fn.filereadable(ftplugin) == 0 then
-			dofile(vim.fn.stdpath("config") .. "/after/ftplugin/default.lua")
-		end
-	end,
+    -- If there's no filetype-specific plugin, source default
+    if vim.fn.filereadable(ftplugin) == 0 then
+      dofile(vim.fn.stdpath("config") .. "/after/ftplugin/default.lua")
+    end
+  end,
 })
+local hardmode = true
+if hardmode then
+  -- Show an error message if a disabled key is pressed
+  local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+
+  -- Disable arrow keys in insert mode with a styled message
+  vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
+
+  -- Disable arrow keys in normal mode with a styled message
+  vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap("n", "<BS>", msg, { noremap = true, silent = false })
+end
+
 --[[
 local handle = io.popen("poetry env info -p 2> /dev/null")
 
