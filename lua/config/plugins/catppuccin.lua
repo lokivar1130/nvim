58276887323f2330c861f596791f87d9ca527982
loki <lokivar1130@gmail.com>
2025-06-06 10:34:56 +0100

    removed backspace and arrows changed theme

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -5,7 +5,22 @@ return {
 		priority = 1000,
 		lazy = false,
 		config = function()
-			vim.cmd.colorscheme("catppuccin")
+			require("catppuccin").setup({
+				flavour = "frappe",
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin-frappe")
 		end,
 	},
 }
